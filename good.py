696372db021205490974a93bb06f59d306a096e3
root <root@localhost.localdomain>
2016-05-27 15:38:36 +0800

    良心厂商

---
+++ good.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding=utf-8
+# code by 92ez.com
+
+import sqlite3
+import Queue
+import time
+import json
+import sys
+import re
+import os
+
+def get20Rank():
+    try:
+        cx = sqlite3.connect(sys.path[0]+"/wooyun.db")
+        cu = cx.cursor()
+        cu.execute("select count(company) as number,company from record where rank = 20 group by company order by number desc")
+
+        for row in cu.fetchall():
+            print 'company: '+row[1]+' ----> 20Rank count: '+str(row[0])
+
+        cu.close()
+        cx.close()
+    except Exception, e:
+        print e
+
+if __name__ == '__main__':
+    get20Rank()
